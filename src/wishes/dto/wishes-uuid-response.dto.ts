a251b9c982fccc7069d39bd8528510c8591d999e
vnarmDA <Vladyslav.Narmaniia@dataart.com>
2023-08-13 07:28:37 +0200

    implemented RESTful API with wishes endpoints

---
+++ src/wishes/dto/wishes-uuid-response.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Wishes } from "../entities/wishes.entity"
+
+export class WishesUuidResponseDto extends Wishes {
+    @ApiProperty()
+    @IsNotEmpty({ message: 'wishes_uuid should not be empty' })
+    @IsString({ message: 'wishes_uuid must be string' })
+    readonly wishes_uuid: string;
+}
--- src/wishes/wishes.module.ts
+++ src/wishes/wishes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WishesService } from './wishes.service';
 import { WishesController } from './wishes.controller';
+import { Wishes } from './entities/wishes.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Wishes])],
   providers: [WishesService],
   controllers: [WishesController],
 })
